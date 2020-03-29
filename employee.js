288942bf95de81579f796d156d3698aeccf0770f
Yogesh1109 <62807141+Yogesh1109@users.noreply.github.com>
2020-03-29 11:53:12 +0530

    Add files via upload
    updated code.

--- employee.js
+++ employee.js
@@ -1,5 +1,6 @@
 
 function myFunction() {
+	var counter = 1;
 	var numRegex = /[0-9]/;
 	var alphaRegex = /[a-zA-Z]/;
 	var phoneNumRegex = /^[(]{0,1}[0-9]{3}[)]{0,1}[-\s\.]{0,1}[0-9]{3}[-\s\.]{0,1}[0-9]{4}$/;
@@ -25,6 +26,23 @@ function myFunction() {
 		return;
 	}
 
+	//check for duplicates
+	var oTable = document.getElementById('eTable');
+
+	//gets rows of table
+	var rowLength = oTable.rows.length;
+
+	//loops through rows    
+	for (i = 0; i < rowLength; i++){
+
+	   //gets cells of current row
+	   var oCells = oTable.rows.item(i).cells;
+	   var cellVal = oCells.item(0).innerHTML;
+	   if(cellVal == eId){
+	   		alert('Employee already added.');
+	   		return;
+	   } 
+	}
 
 	var tableRef = document.getElementById('eTable').getElementsByTagName('tbody')[0];
 
@@ -52,6 +70,7 @@ function myFunction() {
 	btn.type = "button";
 	btn.className = "btn";
 	btn.value = 'Edit';
+	btn.id='edit' + counter;
 	//btn.onclick = update();
 	btn.addEventListener("click", update);
 	newCell5.appendChild(btn);
@@ -60,9 +79,11 @@ function myFunction() {
 	btn.type = "button";
 	btn.className = "btn";
 	btn.value = 'Delete';
+	btn.id='delete'+counter;
 	btn.addEventListener("click", deleteData);
 	//btn.onclick = deleteData();
 	newCell6.appendChild(btn);
+	counter++;
 	
 
 	
@@ -76,9 +97,99 @@ function myFunction() {
 }
 
 function deleteData(e){
-		alert('delete the row'+ e.className);
+		alert('delete the row'+ e.target.className);
 	}
 
 function update(e){
-	alert('update row' + e.className);
+	//alert('update row' + e.target.className);
+	openPopup(e.target.id);
+}
+
+function openPopup(id){
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById(id);
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+//get the rowId
+var rowId = id.substring(id.length-1, id.length);
+
+populateEditData(rowId);
+
+// When the user clicks the button, open the modal 
+  modal.style.display = "block";
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+  clearEditData();
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+    clearEditData();
+  }
+}
+
+}
+
+function populateEditData(rowId){
+	//gets rows of table
+var oTable = document.getElementById('eTable');
+	var rowLength = oTable.rows.length;
+
+	//loops through rows    
+	if(rowId<=rowLength){
+
+	   //gets cells of current row
+	   var oCells = oTable.rows.item(rowId).cells;
+	   var tableRef = document.getElementById('editTable').getElementsByTagName('tbody')[0];
+	   var newRow   = tableRef.insertRow();
+	   	var newCell1  = newRow.insertCell(0);
+
+			// Append a text node to the cell
+
+	   	var cellVal = oCells.item(0).innerHTML;
+		newCell1.appendChild(document.createTextNode(cellVal));
+
+		// Insert a row in the table at the last row
+		
+	   for(i=1; i<oCells.length-2; i++){
+	   		var cellVal = oCells.item(i).innerHTML;
+	   		var newCell1  = newRow.insertCell(i);
+
+			// Append a text node to the cell
+			var textField = document.createElement('input');
+			textField.type = "text";
+			textField.value = cellVal;
+			newCell1.appendChild(textField);
+			//newCell1.appendChild(document.createTextNode(cellVal));
+	   }
+
+	   var updateCell = newRow.insertCell(4);
+	   var btn = document.createElement('input');
+		btn.type = "button";
+		btn.className = "update";
+		btn.value = 'Update';
+		btn.id='update' + rowId;
+		//btn.onclick = update();
+		btn.addEventListener("click", updateData);
+		updateCell.appendChild(btn);
+	   	
+	}
+}
+
+function clearEditData(){
+	document.getElementById("editTable").deleteRow(1);
+}
+
+function updateData(e){
+	alert(e.target.id);
 }
